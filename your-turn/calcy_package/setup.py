62e72ef854f264c6551cb8d75d63ab7327e009b1
nesseri <eric.ness@chrobinson.com>
2020-08-27 10:55:06 -0500

    Your Turn - Python Packages

---
+++ your-turn/calcy_package/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='calcy',
+    version='1.0.0',
+    packages=['calcy'],
+    url='',
+    license='MIT',
+    author='nesseri',
+    author_email='nesseri@chrobinson.com',
+    description='Calculator'
+)
